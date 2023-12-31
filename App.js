35c28b47bdbfac2c6c05bd14126cc2645917ec7d
Hari Yadav <h.hariy2k@gmail.com>
2023-12-31 19:54:54 +0530

    started reactJS and SJX

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
-/**
- * <div id="parent">
- *  <div id="child1">
- *      <h1>i am h1 tag</h1>
- *      <h2>i am h2 tag</h2>
- *  </div>
- * <div id="child2">
- *      <h1>i am h1 tag</h1>
- *      <h2>i am h2 tag</h2>
- *  </div>
- * 
- * </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", { id: "heading", xyz: "test" }, "Hello World from Ract");
 
-const parent = React.createElement("div", { id: "parent" },
-   [ React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I ma h1 tag"),
-         React.createElement("h2",{},"I ma h2 tag"),
-   ]),
+// const parent = React.createElement("div", { id: "parent" },
+//    [ React.createElement("div", { id: "child1" }, [
+//         React.createElement("h1", {}, "I ma h1 tag"),
+//          React.createElement("h2",{},"I ma h2 tag"),
+//    ]),
    
-   React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I ma h1 tag"),
-         React.createElement("h2",{},"I ma h2 tag"),
-    ])]
-);
+//    React.createElement("div", { id: "child2" }, [
+//         React.createElement("h1", {}, "I ma h1 tag"),
+//          React.createElement("h2",{},"I ma h2 tag"),
+//     ])]
+// );
 
+
+const Title = () => (<h1 id="heading" className="head class" tabIndex={1}>This is come from jsx </h1>)
+
+const number = 1000;
+
+const HeadingComponent = () => (
+     <div id="container">
+          <h2>{number}</h2>
+          <Title/>
+          <h1 className="header"> This is Functional component </h1>
+
+     </div>
+);
 
 
 const rootx = ReactDOM.createRoot(document.getElementById("root"));
 
  
-rootx.render(parent);
+rootx.render(<HeadingComponent/>);
